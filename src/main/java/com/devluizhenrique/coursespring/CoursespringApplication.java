b0a6719629d1917080960b028549ebe7096bc87a
Luiz Henrique E. N. Santos <luizhenrique.egydio@gmail.com>
2026-03-29 09:11:56 -0300

    initial commit

---
+++ src/main/java/com/devluizhenrique/coursespring/CoursespringApplication.java
@@ -0,0 +1,13 @@
+package com.devluizhenrique.coursespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursespringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursespringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devluizhenrique/coursespring/CoursespringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devluizhenrique.coursespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursespringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
