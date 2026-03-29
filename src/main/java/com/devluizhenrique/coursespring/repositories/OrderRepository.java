502d58e12483ffe92f5df096c850c206b84fab9c
Luiz Henrique E. N. Santos <luizhenrique.egydio@gmail.com>
2026-03-29 18:58:33 -0300

    Order, Instant, ISO 8601 pt 2

---
+++ src/main/java/com/devluizhenrique/coursespring/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.devluizhenrique.coursespring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devluizhenrique.coursespring.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+}
