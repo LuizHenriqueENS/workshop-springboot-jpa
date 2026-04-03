50dcf206df216e791142d44f3907c9c654b9f9c6
Luiz Henrique E. N. Santos <luizhenrique.egydio@gmail.com>
2026-04-03 16:52:15 -0300

    Payment, one-to-one association

--- src/main/java/com/devluizhenrique/coursespring/config/TestConfig.java
+++ src/main/java/com/devluizhenrique/coursespring/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.devluizhenrique.coursespring.entities.Category;
 import com.devluizhenrique.coursespring.entities.Order;
 import com.devluizhenrique.coursespring.entities.OrderItem;
+import com.devluizhenrique.coursespring.entities.Payment;
 import com.devluizhenrique.coursespring.entities.Product;
 import com.devluizhenrique.coursespring.entities.User;
 import com.devluizhenrique.coursespring.entities.enums.OrderStatus;
@@ -79,6 +80,11 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
