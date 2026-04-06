71a9462b51a1a97a138af309e67854a7c00bf1e6
Luiz Henrique E. N. Santos <luizhenrique.egydio@gmail.com>
2026-04-05 22:11:50 -0300

    Exception handling - update

--- src/main/java/com/devluizhenrique/coursespring/services/UserService.java
+++ src/main/java/com/devluizhenrique/coursespring/services/UserService.java
@@ -13,6 +13,8 @@ import com.devluizhenrique.coursespring.repositories.UserRepository;
 import com.devluizhenrique.coursespring.services.exceptions.DatabaseException;
 import com.devluizhenrique.coursespring.services.exceptions.ResourcesNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -45,9 +47,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = userRepository.getReferenceById(id);
-		updateData(entity, obj);
-		return userRepository.save(entity);
+		try {
+			User entity = userRepository.getReferenceById(id);
+			updateData(entity, obj);			
+			return userRepository.save(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourcesNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
